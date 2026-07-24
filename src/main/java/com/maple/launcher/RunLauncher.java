6e2321237ec1b13c689766b7d04a525655373651
Maplized <260001826+HasanatMomin@users.noreply.github.com>
2026-07-24 20:51:52 +0600

    Update RunLauncher.java

--- src/main/java/com/maple/launcher/RunLauncher.java
+++ src/main/java/com/maple/launcher/RunLauncher.java
@@ -2,7 +2,7 @@ package com.maple.launcher;
 
 public class RunLauncher {
     public static void main(String[] args) {
-        // Delegates execution to the JavaFX Application class
-        MainApp.main(args);
+        // Delegates execution to Main class
+        Main.main(args);
     }
 }
