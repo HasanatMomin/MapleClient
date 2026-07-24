28cb7baff7d5316677f4691b839843a2b8a2aaf0
Maplized <260001826+HasanatMomin@users.noreply.github.com>
2026-07-24 20:52:05 +0600

    Update Main.java

--- src/main/java/com/maple/launcher/Main.java
+++ src/main/java/com/maple/launcher/Main.java
@@ -1,27 +1,30 @@
 package com.maple.launcher;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.InputStream;
+
 public class Main extends Application {
+
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/root.fxml"));
-        Scene scene = new Scene(loader.load());
-        scene.getStylesheets().add(getClass().getResource("/style-dark.css").toExternalForm());
-        
-        stage.setTitle("Maple Launcher");
-        try {
-            stage.getIcons().add(new Image(getClass().getResourceAsStream("/icon.png")));
-        } catch (Exception ignored) {}
-        
-        stage.setMinWidth(1100);
-        stage.setMinHeight(680);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("MapleLauncher");
+
+        // Set Window & Taskbar Icon from src/main/resources/icon.png
+        InputStream iconStream = getClass().getResourceAsStream("/icon.png");
+        if (iconStream != null) {
+            primaryStage.getIcons().add(new Image(iconStream));
+        }
+
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 800, 500);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
